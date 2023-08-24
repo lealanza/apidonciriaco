49bc5c3041291c719d694c039382567df726f01b
Leandro <99040671+lealanza@users.noreply.github.com>
2023-08-24 10:19:25 -0300

    throw new Error(`${email} already exist`);     }

--- helpers/validationDb.ts
+++ helpers/validationDb.ts
@@ -1,10 +1,10 @@
 import { sendEmail } from "../mailers/mailers";
 import User, { IUser } from "../models/users";
 
-export const existingEmail = async (email: string) => {
+export const existingEmail = async (email: string):Promise<void>  => {
     const existEmail:IUser | null = await User.findOne({ email });
     if(existEmail && existEmail.verified){
-        throw new Error("Email already exist");
+        throw new Error(`${email} already exist`);
     }
     if(existEmail && !existEmail.verified){
         await sendEmail(email, existEmail.name as string, existEmail.code as string);
