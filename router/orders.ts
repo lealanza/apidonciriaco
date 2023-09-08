9029989ee237555b9518f82e421abc51a1ab2afc
Leandro <99040671+lealanza@users.noreply.github.com>
2023-09-08 19:10:05 -0300

    route/order/create/check

--- router/orders.ts
+++ router/orders.ts
@@ -6,16 +6,7 @@ import { errorHandler } from "../middlewares/errores";
 const orderRoutes = Router();
 
 orderRoutes.get("/get",errorHandler, getOrders);
-orderRoutes.post("/create",[
-    check('name', 'El nombre es obligatorio').not().isEmpty(),
-    check('cellphone', 'El celular es obligatorio').not().isEmpty(),
-    check('email', 'El email es obligatorio').not().isEmpty(),
-    check('direction', 'La direccion es obligatoria').not().isEmpty(),
-    check('city', 'La ciudad es obligatoria').not().isEmpty(),
-    check('state', 'El estado es obligatorio').not().isEmpty(),
-    check('postalCode', 'El codigo postal es obligatorio').not().isEmpty(),
-    check('products', 'Los productos son obligatorios').not().isEmpty(),
-],errorHandler, createOrder)
+orderRoutes.post("/create",errorHandler, createOrder)
 orderRoutes.delete("/delete/:id",errorHandler, deleteOrder)
 orderRoutes.post("/status/:id",[
     check('status', 'El estado es obligatorio').not().isEmpty(),
