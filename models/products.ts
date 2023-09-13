7ffb503af715007f82db54b1e3bf26ca897ec71d
Leandro <tucholanza@gmail.com>
2023-09-13 19:15:45 -0300

    check

--- models/products.ts
+++ models/products.ts
@@ -1,30 +1,24 @@
 import mongoose, {Model, model, Schema} from 'mongoose'
-import {ICategory} from './categories'
 
-interface Category extends ICategory{
-    name:string;
-}
 export interface IProduct{
     _id:string;
     title:String;
     price:Number;
     ganancia:Number;
-    finalPrice:String;
     stock:Number;
     images:string[];
     description:String;
     date:Date;
-    category:Category;
+    category:String;
 }
 const productSchema = new Schema<IProduct>({
     title:{type:String, required:true},
     price:{type:Number, required:true},
     ganancia:{type:Number, required:true},
-    finalPrice:{type:String},
     stock:{type:Number, required:true},
     images:[{type:String, required:true}],
     description:{type:String, required:true},
-    category:{type:Schema.Types.ObjectId, ref:'Category', required:true},
+    category:{type:String, required:true},
     date:{type:Date, default:Date.now}
 })
 
